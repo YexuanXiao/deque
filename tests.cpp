63bb923aaef5a08b7e9359711043dbc12414e43e
YexuanXiao <bizwen@nykz.org>
2025-03-14 21:10:25 +0800

    combine tests and more tests

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,12 @@
 
 
 #include <cassert>
+#include <deque>
 #include <initializer_list>
 #include <numeric>
 #include <ranges>
 #include <vector>
-#include <deque>
+
 #if !defined(__cpp_size_t_suffix) || __cpp_size_t_suffix < 202011L
 // make IntelliSence happy
 inline constexpr std::size_t operator"" uz(unsigned long long const value) noexcept
@@ -95,7 +96,7 @@ void test_constructor(std::size_t count = 1000uz)
                 assert(d1[idx] == 0x7Euz);
                 assert(v == 0x7Euz);
             }
-            // forward iterator variant are equivalent to emplace_back
+            // forward/bidirectional iterator variant are equivalent to emplace_back
         }
         // (6) equivalent to (4), (5) and operator= (1)
         {
@@ -140,27 +141,94 @@ void test_operator_assign(std::size_t count = 1000uz)
         }
     }
     // (2) equivalent to swap
+    {
+        deque d(100uz);
+        deque d1{};
+        d1 = d;
+        assert(d1.size() == 100uz);
+        d = d1;
+        assert(d.size() == 100uz);
+    }
     // (3) equivalent to (1) and constructor (5.1)
+    {
+        deque d(100uz);
+        d = {0uz, 1uz, 2uz, 3uz};
+        assert(d.size() == 4uz);
+    }
 }
 
 // assign
-// (1) equivalent to constructor (4)
-// (2) equivalent to constructor (5)
-// (3) equivalent to constructor (11)
+template <typename deque>
+void test_assign(std::size_t count = 1000uz)
+{
+    using namespace std::ranges;
+    // (1) equivalent to constructor (4)
+    {
+        deque d(100uz);
+        d.assign(100uz,1uz);
+        assert(d.size() == 100uz);
+    }
+    // (2) equivalent to constructor (5)
+    {
+        deque d(100uz);
+        deque d1(100uz);
+        d1.assign(d.begin(), d.end());
+        assert(d1.size() == 100uz);
+    }
+    // (3) equivalent to constructor (11)
+    {
+        deque d(100uz);
+        deque d1(100uz);
+        d1.assign({0uz, 1uz, 2uz, 3uz});
+        assert(d1.size() == 4uz);
+    }
+}
 
 // assign_range
 // equivalent to assign
+
+template <typename deque>
+void test_assign_range(std::size_t count = 1000uz)
+{
+    deque d{};
+    auto ilist = {0uz, 1uz, 2uz, 3uz};
+    d.assign_range(ilist);
+    assert(d.size() == 4uz);
+}
 
 // operator at tests in other tests above
 // at equivalent to operator at
 
 // front/back never fail
+template <typename deque>
+void test_front_back(std::size_t count = 1000uz)
+{
+    deque d(1uz);
+    d.front();
+    d.back();
+    deque const &d1 = d;
+    d1.front();
+    d1.back();
+}
 
 // size/empty tests in constructor's above and others
 
 // shrink_to_fit never fail
+template <typename deque>
+void test_shrink(std::size_t count = 1000uz)
+{
+    deque d{};
+    d.shrink_to_fit();
+}
 
 // clear tests in constructor (3) and others
+template <typename deque>
+void test_clear(std::size_t count = 1000uz)
+{
+    deque d{100uz};
+    d.clear();
+    assert(d.empty());
+}
 
 // todo: emplace、insert、insert_range、emplace、erase、
 
@@ -274,33 +342,44 @@ void test_emplace_front(std::size_t count = 1000uz)
     }
 }
 
-template <typename deque>
-void test_all(std::size_t count = 1000uz){
-    test_constructor<deque>(count);
-    test_operator_assign<deque>(count);
-    test_emplace_back<deque>(count);
-    test_emplace_front<deque>(count);
+// pop_back/pop_front tests in emplace_back/emplace_front
+
+template <typename Type>
+void test_all(std::size_t count = 1000uz)
+{
+    {
+        test_constructor<std::deque<Type>>(count);
+        test_operator_assign<std::deque<Type>>(count);
+        test_assign<std::deque<Type>>(count);
+        test_assign_range<std::deque<Type>>(count);
+        test_front_back<std::deque<Type>>(count);
+        test_shrink<std::deque<Type>>(count);
+        test_clear<std::deque<Type>>(count);
+        test_emplace_back<std::deque<Type>>(count);
+        test_emplace_front<std::deque<Type>>(count);
+    }
+    {
+        test_constructor<bizwen::deque<Type>>(count);
+        test_operator_assign<bizwen::deque<Type>>(count);
+        test_assign<bizwen::deque<Type>>(count);
+        test_assign_range<bizwen::deque<Type>>(count);
+        test_front_back<bizwen::deque<Type>>(count);
+        test_shrink<bizwen::deque<Type>>(count);
+        test_clear<bizwen::deque<Type>>(count);
+        test_emplace_back<bizwen::deque<Type>>(count);
+        test_emplace_front<bizwen::deque<Type>>(count);
+    }
 }
 
 int main()
 {
-    test_all<bizwen::deque<vsn<1uz>>>();
-    test_all<bizwen::deque<vsn<2uz>>>();
-    test_all<bizwen::deque<vsn<3uz>>>();
-    test_all<bizwen::deque<vsn<4uz>>>();
-    test_all<bizwen::deque<vsn<5uz>>>();
-    test_all<bizwen::deque<vsn<6uz>>>();
-    test_all<bizwen::deque<vsn<7uz>>>();
-    test_all<bizwen::deque<vsn<8uz>>>();
-    test_all<bizwen::deque<vsn<9uz>>>();
-
-    test_all<std::deque<vsn<1uz>>>();
-    test_all<std::deque<vsn<2uz>>>();
-    test_all<std::deque<vsn<3uz>>>();
-    test_all<std::deque<vsn<4uz>>>();
-    test_all<std::deque<vsn<5uz>>>();
-    test_all<std::deque<vsn<6uz>>>();
-    test_all<std::deque<vsn<7uz>>>();
-    test_all<std::deque<vsn<8uz>>>();
-    test_all<std::deque<vsn<9uz>>>();
+    test_all<vsn<1uz>>();
+    test_all<vsn<2uz>>();
+    test_all<vsn<3uz>>();
+    test_all<vsn<4uz>>();
+    test_all<vsn<5uz>>();
+    test_all<vsn<6uz>>();
+    test_all<vsn<7uz>>();
+    test_all<vsn<8uz>>();
+    test_all<vsn<9uz>>();
 }
